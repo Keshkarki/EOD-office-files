05f5b4ce9fc2e02e5d024c18cbb9428d221e21fc
Keshkarki <keshavkarki140@gmail.com>
2023-05-28 02:01:47 +0530

    pnl_file navneet sir done code all set

--- Navneet_datascrap/rough.py
+++
@@ -1,6 +0,0 @@
-import os
-import pandas as pd
-pd.set_option('display.max_rows',None)
-folder_path = "C:\\keshav\\Navneet_datascrap\\newfiles\\all_files"
-file_list = os.listdir(folder_path)
-file_list
